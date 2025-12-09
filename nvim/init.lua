9ec123399af4f7b566f31ea1cd8b8e35febc6cef
willyvamp <hwillpham@gmail.com>
2025-12-09 08:27:01 +0700

    add nvim-tree stuff

--- nvim/init.lua
+++ nvim/init.lua
@@ -31,3 +31,10 @@ vim.opt.tabstop = 2
 vim.opt.expandtab = true
 vim.opt.shiftwidth = 2
 vim.opt.tabstop = 2
+
+-- nvim-tree key bindings
+vim.keymap.set('n', '<leader>e', ':NvimTreeToggle<CR>', { silent = true })
+vim.keymap.set('n', '<C-h>', '<C-w>h', { silent = true })
+vim.keymap.set('n', '<C-j>', '<C-w>j', { silent = true })
+vim.keymap.set('n', '<C-k>', '<C-w>k', { silent = true })
+vim.keymap.set('n', '<C-l>', '<C-w>l', { silent = true })
