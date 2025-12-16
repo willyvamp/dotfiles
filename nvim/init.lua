159d94b006d3fff4ffdb94c21aef54c255f67e33
willyvamp <hwillpham@gmail.com>
2025-12-17 06:28:45 +0700

    add telescope plugins and some lsp stuff

--- nvim/init.lua
+++ nvim/init.lua
@@ -47,10 +47,10 @@ vim.keymap.set('n', '<leader>e', ':NvimTreeToggle<CR>', { silent = true })
 -- vim.keymap.set('n', '<C-k>', '<C-w>k', { silent = true })
 -- vim.keymap.set('n', '<C-l>', '<C-w>l', { silent = true })
  
--- -- telescope key bindings
--- local builtin = require('telescope.builtin')
--- vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
--- vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
--- vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
--- vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+-- telescope key bindings
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 
