e90149cf1af14a952f303a0f0abb1b2dc6e489cd
willyvamp <hwillpham@gmail.com>
2026-05-31 21:28:16 +0700

    change font

--- doom/.doom.d/config.el
+++ doom/.doom.d/config.el
@@ -26,8 +26,8 @@
 ;;(setq doom-font (font-spec :family "Fira Code" :size 12 :weight 'semi-light)
 ;;      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13))
 ;;
-(setq doom-font (font-spec :size 13))
-(setq doom-symbol-font (font-spec :family "Adwaita Mono":size 12))
+(setq doom-font (font-spec :family "JetBrainsMono Nerd Font Mono":size 13))
+(setq doom-symbol-font (font-spec :family "JetBrainsMono Nerd Font Mono":size 13))
 ;;
 ;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
 ;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
